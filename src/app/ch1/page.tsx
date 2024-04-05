3af1ce982ce8ef813cfa72808b4893b0491eb45d
t-whelan <whelan.tionne@gmail.com>
2024-04-05 12:02:36 +0200

    challenge 1

---
+++ src/app/ch1/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+const Ch1 = () =>{
+   return (
+    <div className='bg-teal-800'>
+        <h1 className=' bg-teal-800 text-white text-lg font-bold'>Lorem ipsum dolor sit </h1>
+        <p className='w-[50%] text-white '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti omnis inventore a voluptatibus. In quod earum repellat asperiores fugiat possimus dolorem ipsam reprehenderit iste. Necessitatibus, harum! Consectetur odit facere molestiae.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda delectus quis veniam eos accusamus impedit minima quam labore quo optio, quia maiores dolores facere velit magni aut officiis architecto dolorem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veniam accusantium maxime officiis! Distinctio minus rem tempora eaque nisi. A vitae perspiciatis dolorum assumenda ab dignissimos omnis. Aliquid, ipsam tenetur.
+        </p>
+    </div>
+   )
+}
+
+
+
+
+
+
+export default Ch1
