c74a5b584197d7bec7d8552e88fc64b73b1063ad
t-whelan <whelan.tionne@gmail.com>
2024-04-05 14:01:57 +0200

    challenge 3 section done

---
+++ src/app/ch2/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Ch2 = () =>{
+    return(
+     <div className=' bg-teal-800 text-white'>
+        <h1 className='text-lg font-semibold py-8 w-[50%] mx-auto -mb-10' >Lorem ipsum dolor, sit </h1>
+        <p className='text-white py-8 w-[50%] mx-auto '>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Soluta hic laudantium obcaecati error aliquam laborum ullam fuga modi! Labore vitae aperiam culpa ea. 
+            Voluptas dicta suscipit dolor qui, laudantium quidem!</p>
+            <div className='bg-white text-black'>
+                <h2 className='text-black py-8 mx-auto w-[50%] font-semibold'>More Content D:</h2>
+                <p className='py-2 mx-auto w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, sed? Voluptatum voluptas nostrum eius, quidem iure eveniet inventore debitis repellendus quibusdam aut recusandae suscipit quisquam impedit, architecto iusto et pariatur.</p>
+                <p className='py-2 mx-auto w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, sed? Voluptatum voluptas nostrum eius, quidem iure eveniet inventore debitis repellendus quibusdam aut recusandae suscipit quisquam impedit, architecto iusto et pariatur.</p>
+                <p className='py-2 mx-auto w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, sed? Voluptatum voluptas nostrum eius, quidem iure eveniet inventore debitis repellendus quibusdam aut recusandae suscipit quisquam impedit, architecto iusto et pariatur.</p>
+                <p className='py-2 mx-auto w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, sed? Voluptatum voluptas nostrum eius, quidem iure eveniet inventore debitis repellendus quibusdam aut recusandae suscipit quisquam impedit, architecto iusto et pariatur.</p>
+              
+            </div>
+</div>   
+
+    
+    )
+    
+}
+
+export default Ch2
