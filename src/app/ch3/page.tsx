d5a6154ec677b7fbcbfaf47660d8941a9bcf298b
t-whelan <whelan.tionne@gmail.com>
2024-04-05 14:39:18 +0200

    challenge 3 completed

---
+++ src/app/ch3/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ const Ch3 = () =>{
+    return(
+        <div className='bg-teal-900 text-white'>
+            <h1 className=' text-white font-extrabold text-2xl w-[80%] ml-20 py-10 -mb-10'>Responsive layouts don&apos;t have to be a struggle</h1>
+            <p className='font-light text-md ml-20 mt-5  w-1/2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Ipsa rem aliquid, ab tenetur ad, ratione inventore voluptatem dolorem, 
+                quibusdam beatae iusto dignissimos ut quo!
+                 Odio sunt adipisci accusamus iure repudiandae.
+                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                 
+                </p>
+                <button className='bg-sky-500 text-black font-semibold ml-20 mt-9 mb-20 px-4 py-2 rounded-full uppercase'>I want to learn</button>
+        </div>
+    )
+ }
+
+ export default Ch3
