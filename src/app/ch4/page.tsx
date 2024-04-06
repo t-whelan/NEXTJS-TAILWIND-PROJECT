b4c6e326469cfeda319f23fe7a9af3124510beab
t-whelan <whelan.tionne@gmail.com>
2024-04-06 13:35:18 +0200

    challenge 4 completed

---
+++ src/app/ch4/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Ch4 = () =>{
+    return(
+        <div className='bg-teal-900 text-white'>
+        <h1 className=' text-white font-extrabold text-2xl w-[80%] ml-20 py-10 -mb-10'>Responsive layouts don&apos;t have to be a struggle</h1>
+        <p className='font-light text-md ml-20 mt-5  w-1/2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Ipsa rem aliquid, ab tenetur ad, ratione inventore voluptatem dolorem, 
+            quibusdam beatae iusto dignissimos ut quo!
+             Odio sunt adipisci accusamus iure repudiandae.
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+             
+            </p>
+            <button className='bg-sky-500 text-black font-semibold ml-20 mt-9 mb-20 px-4 py-2 rounded-full uppercase'>I want to learn</button>
+   <div className='bg-white flex gap-4'>
+<h3 className='text-purple-800  font-semibold text-2xl py-8'>Cheap
+<p className='w-[90%] text-black font-light text-sm mb-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem molestias laborum maiores, dolore explicabo, non perspiciatis consequuntur veniam provident cupiditate tempora vero asperiores possimus odio, id doloremque dolores temporibus!</p></h3>
+<h3 className='text-purple-800 font-semibold text-2xl py-8'>Quick
+<p className='w-[90%] text-black font-light text-sm mb-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem molestias laborum maiores, dolore explicabo, non perspiciatis consequuntur veniam provident cupiditate tempora vero asperiores possimus odio, id doloremque dolores temporibus!</p></h3>
+<h3 className='text-purple-800 font-semibold text-2xl py-8'>Qaulity
+<p className='w-[90%] text-black font-light text-sm mb-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem molestias laborum maiores, dolore explicabo, non perspiciatis consequuntur veniam provident cupiditate tempora vero asperiores possimus odio, id doloremque dolores temporibus!</p></h3>
+
+   </div>
+   <div className='bg-teal-900 flex gap-4 text-white'>
+   <h3 className='text-center text-white font-semibold text-2xl py-8 mx-auto w-1/2'>Cheap
+   <p className='w-[90%] text-white font-light text-sm mb-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem molestias laborum maiores, dolore explicabo, non perspiciatis consequuntur veniam provident cupiditate tempora vero asperiores possimus odio, id doloremque dolores temporibus!</p></h3>
+   <h3 className=' py-8 text-center text-white font-semibold text-2xl p-10 mx-auto  w-1/2'>Quick
+   <p className='w-[90%] text-white font-light text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem molestias laborum maiores, dolore explicabo, non perspiciatis consequuntur veniam provident cupiditate tempora vero asperiores possimus odio, id doloremque dolores temporibus!</p></h3>
+   </div>
+   
+    </div>
+    )
+}
+
+export default Ch4
